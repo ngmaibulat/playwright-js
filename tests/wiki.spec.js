d039458ffc8abdafc3303cd99d3ad39cb7b9b69b
ngm <ngmaibulat@gmail.com>
2023-07-27 20:28:43 +0300

    initial

---
+++ tests/wiki.spec.js
@@ -0,0 +1,13 @@
+import { test, expect } from '@playwright/test'
+
+test('test', async ({ page }) => {
+    await page.goto('https://www.wikipedia.org/')
+    await page.getByLabel('Search Wikipedia').click()
+    await page.getByLabel('Search Wikipedia').fill('samsun')
+    await page.getByRole('button', { name: 'Search' }).click()
+    await page
+        .getByRole('link', {
+            name: 'Clockwise from top right: Samsunum-1 ship and coast, Statue of Honor, Atatürk Culture Centre, Bandırma Ferry and National Struggle Park Open Air Museum, Saathane Square, Store 55',
+        })
+        .click()
+})
